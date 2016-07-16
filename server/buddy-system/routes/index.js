e8975eb30fa605a4bbd9da415f2b5f40b8b0d80a
Tomas Barry <tomas.barry@hotmail.com>
2016-07-15 21:10:16 -0700

    Home end point base code added

--- server/buddy-system/routes/index.js
+++ server/buddy-system/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res, next) {
+	console.log('home route start point');
+	var userID = req.query.userID;
+
+	// Fetch profile
+
+	// Fetch pending tasks
+	// Fetch buddy tasks
+
+	// Fetch image for graph
+
+	// Render view for homepage
+	console.log('home route end point');
+
+});
+
 module.exports = router;
