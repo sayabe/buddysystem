04d8bd09506db6f0dbc299540012c68874e2da1b
jayasudha jayakumaran <jjayakum@andrew.cmu.edu>
2016-07-15 21:47:18 -0700

    added new endpoints

--- server/buddy-system/routes/index.js
+++ server/buddy-system/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/home', function(req, res, next) {
+router.get('/home/:userID', function(req, res, next) {
 	console.log('home route start point');
 	var userID = req.query.userID;
 
@@ -21,5 +21,55 @@ router.get('/home', function(req, res, next) {
 	console.log('home route end point');
 
 });
+
+router.get('/profile/:userID',function(req,res.next){
+	console.log('profile route start point');
+	var userID = req.query.userID;
+
+	//fetch profile 
+
+	//fetch tasks
+	//fetch graph
+
+	//add task 
+	console.log('profile route end point');
+
+});
+
+router.get('/yourBuddyList',function(req,res,next){
+	console.log('buddy list start point');
+	var userID = req.query.userID;
+
+	//fetch all your buddylists
+
+	console.log('buddy list end point');
+});
+
+
+
+router.get('/addNewTask',function(req,res,next){
+	console.log('addNewTask start point');
+	
+
+	//render the form
+
+
+	console.log('addNewTask end point');
+});
+
+router.post('/addNewTask',function(req,res,next){
+
+	console.log('addNewTask post start point');
+
+	/*write to db.
+	Task name
+	start date
+	deadline
+	reward = none */
+
+	console.log('addNewTask post start point');
+});
+
+
 
 module.exports = router;
