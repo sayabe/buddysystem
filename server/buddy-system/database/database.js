5fb4339c6075fd0fbfb2ae4a5f74d0b57bb87777
Tomas Barry <tomas.barry@hotmail.com>
2016-07-15 21:29:09 -0700

    Added dummy functions for database

---
+++ server/buddy-system/database/database.js
@@ -0,0 +1,18 @@
+// var mongo = require('mongo');
+
+var controller = {};
+
+var myFunction = function() {
+
+}
+function myOtherFunction() {
+
+}
+
+controller.addTask = function(taskNome, startTime, endTime, reward) {
+	console.log(taskNome);
+	myFunction();
+	myOtherFunction();
+};
+
+module.exports = controller;
--- server/buddy-system/routes/index.js
+++ server/buddy-system/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var database = require('../database/database');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,13 +11,14 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
 	console.log('home route start point');
 	var userID = req.query.userID;
+	database.addTask('run', 0, 0, 0);
 
 	// Fetch profile
 
 	// Fetch pending tasks
 	// Fetch buddy tasks
 
-	// Fetch image for graph
+	// Fetch random buddies
 
 	// Render view for homepage
 	console.log('home route end point');
